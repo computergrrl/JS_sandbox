d17b0ef0363f9dfb3b5516a1db7f9b4abd8aeee2
Amber Stevens <anani.mowse@gmail.com>
2019-04-17 10:08:10 -0700

    Creating elements works like expected

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
-let newItem = document.createElement('li');
+
 let button1 = document.getElementById('button1');
 let input1 = document.getElementById('input1');
-let list1 = document.querySelector('list1');
+let list1 = document.getElementById('list1');
 
 button1.addEventListener('click' , () => {
-
+      let newItem = document.createElement('li');
+      newItem.textContent = input1.value;
       list1.appendChild(newItem);
-      
+      input1.value = '';
+
 
 });
